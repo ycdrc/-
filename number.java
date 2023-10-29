d533d5c342be81472b9dd0cea82f628ac7c4d9f1
ycdrc <148563167+ycdrc@users.noreply.github.com>
2023-10-29 23:44:16 +0800

    Add files via upload

---
+++ number.java
@@ -0,0 +1,10 @@
+public class number {
+    public static void main(String[]args){
+        byte a=127;
+        short b=128;
+        int c=100000000;
+        long d=1000000000;
+        long number=a+b+c+d;
+        System.out.println("答案是"+number);
+    }
+}
