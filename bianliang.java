1e4cff4388f3bf65bf5ad7adbad59cfe8e9fe410
ycdrc <148563167+ycdrc@users.noreply.github.com>
2023-10-30 23:35:42 +0800

    Add files via upload

---
+++ bianliang.java
@@ -0,0 +1,18 @@
+public interface bianliang {
+
+    // 成员变量（实例变量）age
+    int age=18;
+
+    public static void main(String[] args) {
+
+
+        // 局部变量name
+        String name = "John";
+        System.out.println("Age (成员变量): " + age); // 访问成员变量age
+        System.out.println("Name (局部变量): " + name); // 访问局部变量name
+    }
+}
+
+
+
+
