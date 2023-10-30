671542526bb73932499ea1cdd6a33efc45aa4199
ycdrc <148563167+ycdrc@users.noreply.github.com>
2023-10-30 21:08:32 +0800

    Add files via upload

---
+++ leixingzhuanhuan.java
@@ -0,0 +1,13 @@
+public class leixingzhuanhuan {
+    public static void main(String[] args) {
+        byte a=1;
+        short b=2;
+        long c=3;
+        int d =(int)a ;
+        int e=(int)b;
+        int f=(int)c;
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+    }
+}
