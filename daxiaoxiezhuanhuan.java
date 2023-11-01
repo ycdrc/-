440c212e9ab4f24b07e5f5844428d5cc18a4c828
ycdrc <148563167+ycdrc@users.noreply.github.com>
2023-11-02 00:30:21 +0800

    Add files via upload

---
+++ daxiaoxiezhuanhuan.java
@@ -0,0 +1,11 @@
+import java.util.Locale;
+
+public class daxiaoxiezhuanhuan {
+    public static void main(String[] args) {
+        String chushi ="Hellow world";
+        String daxie=chushi.toUpperCase();
+        System.out.println(daxie);
+        String xioaxie=chushi.toLowerCase();
+        System.out.println(xioaxie);
+    }
+}
---
+++ whileqiuhe.java
@@ -0,0 +1,11 @@
+public class whileqiuhe {
+    public static void main(String[] args) {
+        double a=20,i=1,c=0,b=1;
+        while(i<=a) {
+             b=b/i;
+             c=c+b;
+             i++;
+        }
+        System.out.println("所求之和为"+c);
+    }
+}
